b102b5ad75c0c741516beb4e159cc1ebefb65cd3
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-11 20:09:49 +0100

    works up to 9 letters

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,14 +1,13 @@
 class WordController < ApplicationController
     require 'trie'
-    $trie = Trie.read("word_trie")
+    $trie ||= Trie.read("word_trie")
     def home
     end
 
     def search
-      letters = ["a","g","a","w","u"]
+      letters = params[:q].chars.sort
       combination = (2..letters.length).flat_map{ |n| letters.permutation(n).map(&:join) }.uniq
       @words = []
-      @user_query = params[:q]
       combination.each { |word| @words.push(word) if $trie.has_key?(word) }
     end
 
