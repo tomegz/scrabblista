915dc60c23ce40f65ac6dc1ffffe0d31e35b31d9
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-11 19:07:49 +0100

    add home and search routes, search bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/welcome" => "word#welcome"
+  root "word#home"
+  get "/search" => "word#search"
+  # resources :words
 end
