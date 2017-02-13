9a8dd51944f672a19ee182ff4008c234855ae74e
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-13 20:34:44 +0100

    add letters limit of 20, fix deciphering

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,6 +1,7 @@
 class WordController < ApplicationController
     require 'trie'
     $trie ||= Trie.read("word_trie")
+
     def home
     end
 
@@ -9,12 +10,15 @@ class WordController < ApplicationController
       keys = (2..letters.length).flat_map{ |n| letters.combination(n).map(&:join).map{|n| n = n.length.to_s + n.chars.sort.join } }.uniq
       @words = []
       keys.each { |key| @words.push($trie.children(key)) if $trie.has_children?(key) }
-      @words = @words.flatten.map{ |w| w = key_to_word(w) } 
+      @words.flatten!.map!{ |w| key_to_word(w) } unless @words.empty?
     end
     
+    # private methods
+
     private
     def key_to_word(key)
       a = key.length
-      key.slice(a+1..a)
+      key[a/2+1..a-1]
     end
+
 end
