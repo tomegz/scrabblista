59fd10c0d7ad500f7d05f38df22803554192c2bf
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-13 20:11:28 +0100

    fix words array

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -9,7 +9,7 @@ class WordController < ApplicationController
       keys = (2..letters.length).flat_map{ |n| letters.combination(n).map(&:join).map{|n| n = n.length.to_s + n.chars.sort.join } }.uniq
       @words = []
       keys.each { |key| @words.push($trie.children(key)) if $trie.has_children?(key) }
-      @words.flatten!.each{ |w| w = key_to_word(w) } # can cause bugs?
+      @words = @words.flatten.map{ |w| w = key_to_word(w) } 
     end
     
     private
