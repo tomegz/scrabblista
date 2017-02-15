a1c2402e3327fedb1706c7a8c7d2418a3ad51efc
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-15 14:29:53 +0100

    chnage controller to be able to use blanks with ? mark

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,6 +1,7 @@
 class WordController < ApplicationController
     require 'trie'
     $trie ||= Trie.read("word_trie")
+    $all_letters = %w[a ą b c ć d e ę f g h i j k l ł m n ń o ó p r s ś t u w y z ź ż]
 
     def home
     end
@@ -8,8 +9,13 @@ class WordController < ApplicationController
     def search
       letters = params[:q].gsub(/\s+/, "").chars.sort # needs validations
       keys = (2..letters.length).flat_map{ |n| letters.combination(n).map(&:join).map{|n| n = n.length.to_s + n.chars.sort.join } }.uniq
+      keys_with_blanks = keys.select{ |k| k.include? "?" }.map{ |k| k.gsub("?","") }
+      final_keys = keys_with_blanks.map do |k| 
+              k = $all_letters.map{ |l| l.gsub(l,l+k) }
+             end.flatten.map{ |k| k.chars.sort.join }.uniq
       @words = []
       keys.each { |key| @words.push($trie.children(key)) if $trie.has_children?(key) }
+      final_keys.each { |key| @words.push($trie.children(key)) if $trie.has_children?(key) }
       @words.flatten!.map!{ |w| key_to_word(w) } unless @words.empty?
     end
     
