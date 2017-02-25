dfa81f18103e5a2a3ad8eaa77a157e1e211f2531
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-25 16:19:06 +0100

    check controller

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,39 +1,33 @@
 class WordController < ApplicationController
     require 'trie'
-    $trie ||= Trie.read("word_trie")
-    $all_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "w", "y", "z", "ó", "ą", "ć", "ę", "ł", "ń", "ś", "ź", "ż"]
+    @@trie = Trie.read("full_word_trie")
+    @@alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "w", "y", "z", "ó", "ą", "ć", "ę", "ł", "ń", "ś", "ź", "ż"]
 
     def home
     end
 
     def search
-      @letters = params[:q].gsub(/\s+/, "").chars.sort # needs validations
-      keys = (2..@letters.length).flat_map{ |n| @letters.combination(n).map(&:join).map{|n| n = n.length.to_s + n.chars.sort.join } }.uniq
-      keys_with_blanks = keys.select{ |k| k.include? "?" }.map{ |k| k.gsub("?","") }
-      #final_keys = keys_with_blanks.map do |k| 
-       #       k = $all_letters.map{ |l| l.gsub(l,l+k) }
-       #      end.flatten.map{ |k| k.chars.sort.join }.uniq
-      final_keys = keys_with_blanks.map do |k| 
-        current_letters = choose_letters($all_letters, k)
-        k = current_letters.map{ |l| l.gsub(l,l+k) } 
-      end.flatten.map{ |k| k.chars.sort.join }.uniq
-      final_keys += keys_with_blanks
-      @words = []
-      keys.each { |key| @words.push($trie.children(key)) if $trie.has_children?(key) }
-      final_keys.each { |key| @words.push($trie.children(key)) if $trie.has_children?(key) }
-      @words.flatten!.map!{ |w| key_to_word(w) } unless @words.empty?
+      @input = params[:q].gsub(/\s+/, "").chars.sort # needs validations
+      @wildcards = @input.select{ |c| c=="?" }.size
+      @input = @input.join.gsub("?","").chars if wildcards > 0
+      @output = []
+      search_word(@@trie.root, @wildcards, @@alphabet, @input, @output)
     end
     
     # private methods
 
     private
-    def key_to_word(key)
-      a = key.length
-      key[a/2+1..a-1]
+
+    def take_away(tiles, tile)
+      if tile.is_a? Array
+        tile.each{ |t| tiles.join.sub(t, "") }
+        tiles 
+      else
+        tiles.join.sub(tile, "").chars
+      end
     end
 
-    def choose_letters(letters, key)
-      letters = letters.select{ |l| l <= key.chars.last }
+    def search_word(node, wildcards, alphabet, tiles, output)
+      @output = ["works", "hey"]
     end
-
 end
