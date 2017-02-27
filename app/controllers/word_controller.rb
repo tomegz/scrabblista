70509fc16f010da2ec4cfca5adb147d717b4fbb4
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-27 22:30:30 +0100

    add validations

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -7,9 +7,29 @@ class WordController < ApplicationController
     end
 
     def search
+      validate_input
       @rack = Tile.new(params[:q])
       @scrabble = Scrabble.new(@@trie, @rack)
       @words = @scrabble.find_words
     end
+
+    private
+    def validate_input
+      if params[:q].size < 2 || params[:q].scan(letters_and_blanks).any? || params[:q].count("?") > 2
+        flash[:notice] = "Potrzebujesz co najmniej dwóch liter, aby ułożyć słowo." if params[:q].size < 2 
+        flash[:notice] = "Używaj tylko liter, lub '?' dla blanków." if params[:q].scan(letters_and_blanks).any? 
+        flash[:notice] = "Możesz użyć co najwyżej dwóch '?' - blanków." if params[:q].chars.count("?") > 2
+        render 'home'
+      end
+    end
+
+    def letters_and_blanks
+      /[^a-pA-Pr-uR-U?wyzWYZóąćęłńśźż_]/
+    end
     
 end
+#<%= form_tag("/search", method: "get") do %>
+  #<%= label_tag(:q, "My rack:") %>
+  #<%= text_field_tag 'q', nil, maxlength: 12 %>
+  #<%= submit_tag("Create words from my letters") %>
+#<% end %>
--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,16 +1,10 @@
 class Tile
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
   # validations here!
-  attr_accessor :inputted, :wildcards, :tiles
+  attr_reader :inputted, :wildcards, :tiles
 
-  #validates :inputted, exclusion: { in: %w(# ?) }
-
-  validates :wildcards, inclusion: { in: 2..9 }
 
   def initialize(letters)
-    @inputted = letters.chars.sort
+    @inputted = letters.downcase.chars.sort
     @wildcards = @inputted.select{ |c| c=="?" }.size
     @tiles =  @wildcards > 0 ? @inputted.join.gsub("?","").chars : @inputted
   end
