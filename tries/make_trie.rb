c614fec3b4835fc6e9e8d628404666646cf7525f
Tomasz Bubala <tomaszbubala@o2.pl>
2017-02-27 23:04:39 +0100

    small change

--- tries/make_trie.rb
+++ tries/make_trie.rb
@@ -1,11 +1,9 @@
 require 'trie'
-tries = []
-14.times{ tries.push(Trie.new) }
+trie = Trie.new
 IO.readlines('slowa.txt').each do |word| 
   w = word.chomp
   puts "at najlepszy" if w == "najlepszy"
-  l = w.length - 2
-  tries[l].add(w)
+  trie.add(w.force_encoding("utf-8"))
   #trie.add(w.length.to_s + w.chars.sort.join + w)
   #if trie.get(key).nil?
     #word_or_array = []
@@ -15,7 +13,7 @@ IO.readlines('slowa.txt').each do |word|
   #trie.add(key, word_or_array)
   #w.length.times{ |n| trie.add(w.length.to_s + w.sub(/#{word[n-1]}/,"?").chars.sort.join + word) }
 end
-tries.each_with_index{ |t, i| t.save("#{i}_letter_trie") }
+trie.save("new_trie")
 
 
 
