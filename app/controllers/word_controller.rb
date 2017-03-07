5ef7e4a043721cd6990a8775f6ffbe326eec581c
Tomasz Bubala <tomaszbubala@o2.pl>
2017-03-07 22:41:07 +0100

    change tile model, add new css

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -7,29 +7,24 @@ class WordController < ApplicationController
     end
 
     def search
-      validate_input
       @rack = Tile.new(params[:q])
+      validate_input
       @scrabble = Scrabble.new(@@trie, @rack)
       @words = @scrabble.find_words
     end
 
     private
-    def validate_input # TRY flash.now
-      if params[:q].size < 2 || params[:q].scan(letters_and_blanks).any? || params[:q].count("?") > 2
-        flash[:notice] = "Potrzebujesz co najmniej dwóch liter, aby ułożyć słowo." if params[:q].size < 2 
-        flash[:notice] = "Używaj tylko liter, lub '?' dla blanków." if params[:q].scan(letters_and_blanks).any? 
-        flash[:notice] = "Możesz użyć co najwyżej dwóch '?' - blanków." if params[:q].chars.count("?") > 2
+    def validate_input 
+      if params[:q].size < 2 || forbidden_chars.any? || params[:q].count("?") > 2
+        flash.now[:notice] = "Potrzebujesz co najmniej dwóch liter, aby ułożyć słowo." if params[:q].size < 2 
+        flash.now[:notice] = "Z tych znaków nie ułożysz słowa: #{forbidden_chars.uniq.join(', ')}" if forbidden_chars.any? 
+        flash.now[:notice] = "Możesz użyć co najwyżej dwóch '?' - blanków." if params[:q].chars.count("?") > 2
         render 'home'
       end
     end
 
-    def letters_and_blanks
-      /[^a-pA-Pr-uR-U?wyzWYZóąćęłńśźż\s_]/
+    def forbidden_chars
+      @rack.input.scan(/[^a-pA-Pr-uR-U?wyzWYZóąćęłńśźż_]/)
     end
     
 end
-#<%= form_tag("/search", method: "get") do %>
-  #<%= label_tag(:q, "My rack:") %>
-  #<%= text_field_tag 'q', nil, maxlength: 12 %>
-  #<%= submit_tag("Create words from my letters") %>
-#<% end %>
--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,14 +1,15 @@
 class Tile
   # validations here!
   include Alphabet
-  attr_reader :inputted, :wildcards, :tiles
+  attr_reader :input, :wildcards, :tiles
 
   def initialize(letters)
-    @inputted = letters.downcase
-    P_LETTERS.each { |key, value| @inputted.gsub!(key, value) }
-    @inputted = @inputted.chars.sort
-    @wildcards = @inputted.select{ |c| c=="?" }.size
-    @tiles =  @wildcards > 0 ? @inputted.join.gsub("?","").chars : @inputted
+    @input = letters.downcase.gsub(/\s+/,"")
+    @input_change = letters.downcase.gsub(/\s+/,"")
+    P_LETTERS.each { |key, value| @input_change.gsub!(key, value) }
+    @input_change = @input_change.chars.sort
+    @wildcards = @input_change.select{ |c| c=="?" }.size
+    @tiles = @wildcards > 0 ? @input_change.join.gsub("?","").chars : @input_change
   end
   
 end
